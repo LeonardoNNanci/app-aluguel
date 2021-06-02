ccfd6aaf95f995690c1547a61a0764ec495640a5
LeonardoNNanci <leonardonanci@id.uff.br>
2021-06-02 16:28:04 -0300

    added default icons file

---
+++ lib/style/default_icons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DefaultIcons {
+  static Icon get imovel => Icon(Icons.home_work);
+
+  static Icon get aluguel => Icon(Icons.calendar_today);
+
+  static Icon get hospede => Icon(Icons.person);
+
+  static Icon get despesa => Icon(Icons.construction);
+
+  static Icon get edit => Icon(Icons.edit);
+
+  static Icon get remove => Icon(Icons.delete);
+}
